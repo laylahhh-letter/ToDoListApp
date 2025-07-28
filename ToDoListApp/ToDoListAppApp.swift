623bbf35993da44166cfe09b12443fcc340f857f
Layla <layla.hisham.sami@gmail.com>
2025-07-28 09:56:17 -0700

    Initial Commit

---
+++ ToDoListApp/ToDoListAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListAppApp.swift
+//  ToDoListApp
+//
+//  Created by Layla Hisham on 7/28/25.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
